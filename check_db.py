93465ae2e8bf6f0bb50a2e6b6ac20789a58762cb
wikijm <wikijm@users.noreply.github.com>
2026-05-12 15:18:29 +0200

    feat: add English localization and implement Leboncoin scraper testing utilities

--- check_db.py
+++
@@ -1,9 +0,0 @@
-
-import sqlite3
-conn = sqlite3.connect('immo_boussole.db')
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-conn.close()
